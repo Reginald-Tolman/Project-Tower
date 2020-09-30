e5c560ca1088fdda4ed4f10fc88518caa69d6b6e
Mason <reginald.tolman@outlook.com>
2020-09-30 21:38:54 +0800

    Initial Mass Commit

---
+++ Project Tower/Assets/Scripts/Models/Tower.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : Unit
+{
+    // Start is called before the first frame update
+    Tower()
+    {
+        Health = 100;
+        MoveSpeed = 0;
+        IsStationary = true;        
+    }
+}
---
+++ Project Tower/Assets/Scripts/Models/Unit.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public int Health {get; set; }
+    public int MoveSpeed { get; set; }
+    public bool IsStationary {get; set;}
+}
